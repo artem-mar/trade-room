2422a5ed3dbc233266d8923a4b38e855d8ead43e
Artem M <artemmarin91@mail.ru>
2023-01-07 23:07:06 +0300

    added fetching traders and timer info

--- src/Room.jsx
+++ src/Room.jsx
@@ -1,60 +1,8 @@
 import React, { useState, useMemo, useEffect } from 'react';
+import axios from 'axios';
 import RoomContext from './context.js';
 import Timer from './Timer.jsx';
 import { getCurrentId } from './utils.jsx';
-
-const traders = [
-  {
-    id: 1,
-    name: 'name 1',
-    qualityImprovement: '-',
-    productionTime: '11',
-    warrantyPeriod: '111',
-    paymentTerms: '123',
-    lotProductionCost: '111',
-    actions: '',
-  },
-  {
-    id: 2,
-    name: 'name 2',
-    qualityImprovement: '-',
-    productionTime: '22',
-    warrantyPeriod: '222',
-    paymentTerms: '234',
-    lotProductionCost: '211',
-    actions: '',
-  },
-  {
-    id: 3,
-    name: 'name 3',
-    qualityImprovement: '-',
-    productionTime: '33',
-    warrantyPeriod: '333',
-    paymentTerms: '345',
-    lotProductionCost: '321',
-    actions: '',
-  },
-  {
-    id: 4,
-    name: 'name 4',
-    qualityImprovement: '-',
-    productionTime: '11',
-    warrantyPeriod: '111',
-    paymentTerms: '123',
-    lotProductionCost: '111',
-    actions: '',
-  },
-  {
-    id: 5,
-    name: 'name 5',
-    qualityImprovement: '-',
-    productionTime: '22',
-    warrantyPeriod: '222',
-    paymentTerms: '234',
-    lotProductionCost: '211',
-    actions: '',
-  },
-];
 
 const traderScheme = {
   id: [],
@@ -67,21 +15,9 @@ const traderScheme = {
   actions: [],
 };
 
-// const tradersTable = traders.reduce(
-//   (acc, trader) => ({
-//     names: [...acc.names, trader.name],
-//     qualityImprovements: [...acc.qualityImprovements, trader.qualityImprovement],
-//     productionTimes: [...acc.productionTimes, trader.productionTime],
-//     warrantyPeriods: [...acc.warrantyPeriods, trader.warrantyPeriod],
-//     paymentTerms: [...acc.paymentTerms, trader.paymentTerms],
-//     lotProductionCosts: [...acc.lotProductionCosts, trader.lotProductionCost],
-//     actions: [...acc.actions, trader.actions],
-//   }), traderScheme
-// );
-
 const renderTd = (params) => {
   const {
-    id, key, value, i, activeUserId,
+    id, key, value, i, activeTraderId,
   } = params;
 
   switch (key) {
@@ -96,9 +32,9 @@ const renderTd = (params) => {
       return (
         <td
           key={`${key} ${id}`}
-          className={`text-center px-5 ${activeUserId === id && 'table-danger'}`}
+          className={`text-center px-5 ${activeTraderId === id && 'table-danger'}`}
         >
-          {activeUserId === id && <Timer />}
+          {activeTraderId === id && <Timer />}
         </td>
       );
     default:
@@ -110,44 +46,69 @@ const renderTd = (params) => {
   }
 };
 
-const time = new Date('01-01-2023 00:01').getTime();
-
 const Room = () => {
-  const [activeTraderId, setActiveTraderId] = useState(traders[0].id);
+  const [activeTraderId, setActiveTraderId] = useState(null);
   const [startTime, setStartTime] = useState(Date.now());
-
-  const ids = traders.map(({ id }) => id);
+  const [traders, setTraders] = useState([]);
 
   const setNextTraderId = () => {
+    const ids = traders.map(({ id }) => id);
     const index = ids.indexOf(activeTraderId);
     const newId = index === ids.length - 1 ? ids[0] : ids[index + 1];
     setActiveTraderId(newId);
   };
 
   useEffect(() => {
-    setStartTime(time);
-    setActiveTraderId(getCurrentId(ids, 1, time));
-  }, [startTime]);
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        'https://trade-room-7f3ef-default-rtdb.europe-west1.firebasedatabase.app/roomInfo.json',
+      );
+      setActiveTraderId(
+        getCurrentId(data.traders, data.timerInfo.startUserId, data.timerInfo.startTime),
+      );
+      setStartTime(data.timerInfo.startTime);
+      setTraders(data.traders);
+    };
+    fetchData();
+  }, [startTime, activeTraderId]);
 
-  const tTable = traders.reduce((acc, trader, i) => {
+  const tradersTable = traders.reduce((acc, trader, i) => {
     const entries = Object.entries(trader);
     const { id } = trader;
 
     return entries.reduce(
       (ac, [key, value]) => ({
         ...ac,
-        [key]: [...ac[key], renderTd({
-          id, key, value, i, activeUserId: activeTraderId,
-        })],
+        [key]: [
+          ...ac[key],
+          renderTd({
+            id, key, value, i, activeTraderId,
+          }),
+        ],
       }),
       acc,
     );
   }, traderScheme);
 
+  const switchTimer = async () => {
+    try {
+      const ids = traders.map(({ id }) => id);
+      const index = ids.indexOf(activeTraderId);
+      const newId = index === ids.length - 1 ? ids[0] : ids[index + 1];
+      await axios.put(
+        'https://trade-room-7f3ef-default-rtdb.europe-west1.firebasedatabase.app/roomInfo/timerInfo.json',
+        { startTime: Date.now(), startUserId: newId },
+      );
+      setStartTime(Date.now());
+      setActiveTraderId(newId);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
-    <RoomContext.Provider value={useMemo(() => (
-      { startTime, setNextTraderId }
-    ), [activeTraderId, startTime])}
+    <RoomContext.Provider
+      value={useMemo(() => ({ startTime, setNextTraderId }), [activeTraderId, startTime])}
     >
       <div className="p-4">
         <div className="fs-5 text-danger">
@@ -158,52 +119,54 @@ const Room = () => {
         <hr />
         <table className="table table-striped caption-top fs-6 fw-weight-light w-100">
           <caption className="text-danger">
-            Уважаемые участники, во время вашего хода вы можете изменить параметры торгов,
-            указанных в таблице:
+            Уважаемые участники, во время вашего хода вы можете изменить параметры торгов, указанных
+            в таблице:
           </caption>
           <thead className="text-primary text-uppercase align-middle">
             <tr>
               <td className="min-vw-25">Ход</td>
-              {tTable.id}
+              {tradersTable.id}
             </tr>
             <tr>
               <td>Параметры и требования</td>
-              {tTable.name}
+              {tradersTable.name}
             </tr>
           </thead>
           <tbody className="table-group-divider align-middle">
             <tr>
               <td>Наличие комплекса мероприятий, повышающих стандарты качества изготовления</td>
-              {tTable.qualityImprovement}
+              {tradersTable.qualityImprovement}
             </tr>
 
             <tr>
               <td>Срок изготовления лота, дней</td>
-              {tTable.productionTime}
+              {tradersTable.productionTime}
             </tr>
 
             <tr>
               <td>Гарантийные обязательства, мес</td>
-              {tTable.warrantyPeriod}
+              {tradersTable.warrantyPeriod}
             </tr>
 
             <tr>
               <td>Условия оплаты</td>
-              {tTable.paymentTerms}
+              {tradersTable.paymentTerms}
             </tr>
 
             <tr>
               <td>Стоимость изготовления лота, руб (без НДС)</td>
-              {tTable.lotProductionCost}
+              {tradersTable.lotProductionCost}
             </tr>
 
             <tr>
               <td>Действия</td>
-              {tTable.actions}
+              {tradersTable.actions}
             </tr>
           </tbody>
         </table>
-        <button onClick={() => setNextTraderId()} type="button" className="btn btn-primary">btn</button>
+        <button onClick={switchTimer} type="button" className="btn btn-primary">
+          Сделать ход
+        </button>
       </div>
     </RoomContext.Provider>
   );
